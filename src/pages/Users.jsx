16a590482c3a982f704eef800aa7311335e93930
trustdubaiae-ship-it <trustdubai.ae@gmail.com>
2026-05-29 14:57:16 +0400

    Create Users.jsx

---
+++ src/pages/Users.jsx
@@ -0,0 +1,8 @@
+export default function Users() {
+  return (
+    <div>
+      <h2 style={{ fontSize: 20, fontWeight: 600, marginBottom: 8 }}>Users</h2>
+      <p style={{ color: '#6b7280', fontSize: 14 }}>All registered reviewers and customers. Coming soon.</p>
+    </div>
+  )
+}
